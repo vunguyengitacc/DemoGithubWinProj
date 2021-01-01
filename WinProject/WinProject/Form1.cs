f0bc9566c3de110a808c766601e0ba7138bb9ca4
nguyenhoanganhvu <nguyenhoanganhvu.it@gmail.com>
2021-01-01 10:11:23 +0700

    login form

--- WinProject/WinProject/Form1.cs
+++ WinProject/WinProject/Form1.cs
@@ -22,5 +22,46 @@ namespace WinProject
             Form2 nf = new Form2();
             nf.ShowDialog();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "username" && txtPassword.Text == "pass")
+            {
+                Form2 ifPage = new Form2();
+                ifPage.ShowDialog();
+            }
+            else
+            {
+                DialogResult dr = MessageBox.Show("Tên tài khoản hoặc mật khẩu không chính xác!\nBạn có muốn nhập lại?", "Lỗi nhập", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    txtUsername.Text = "";
+                    txtPassword.Text = "";
+                }
+            }
+        }
+
+        private void checkLengTextBox(object sender, TextBox txtBox)
+        {
+            if (txtBox.Text.Length > 20)
+            {
+                MessageBox.Show("Không được nhập quá 20 kí tự", "Lỗi nhập");
+                txtBox.Text = "";
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            checkLengTextBox(sender, txtUsername);
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            checkLengTextBox(sender, txtPassword);
+        }
     }
 }
